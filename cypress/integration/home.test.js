1ba034076bad9e6b616acb9d7768101cae0f3359
Hari <hariar2.0@outlook.com>
2020-04-30 23:10:00 -0600

    Added E2e test cases for home page using Cypress

--- cypress/integration/home.test.js
+++ cypress/integration/home.test.js
@@ -1,64 +1,40 @@
+describe("Home Page", () => {
+  beforeEach(() => {
+    // First we need to mock up the data using courses.json and save them in courseJSON.
+    cy.fixture("courses.json").as("courseJSON");
 
+    // call the mock server
+    cy.server();
 
+    // Next we call API endpoint and result will save in courseJSON. @ is to represent the payload.
+    cy.route("/api/courses", "@courseJSON").as("courses");
 
-describe('Home Page', () => {
+    //Visit the main page.
+    cy.visit("/");
+  });
 
-    beforeEach(() => {
+  it("should display list of courses", () => {
+    //Check the page- Page contains All Courses text.
+    cy.contains("All Courses");
 
-        cy.fixture('courses.json').as("coursesJSON");
+    // wait for the courses data
+    cy.wait("@courses");
+    cy.get("mat-card").should("have.length", 9);
+  });
 
-        cy.server();
+  it("should display advanced courses", () => {
+    // Should check home page contains 2 tabs (Beginners and Advanced)
+    cy.get(".mat-tab-label").should("have.length", 2);
 
-        cy.route('/api/courses', "@coursesJSON").as("courses");
+    //Click the Advanced tab.
+    cy.get(".mat-tab-label").last().click();
 
-        cy.visit('/');
+    cy.get(".mat-tab-body-active .mat-card-title")
+      .its("length")
+      .should("be.gt", 1);
 
-    });
-
-    it('should display a list of courses', () => {
-
-        cy.contains("All Courses");
-
-        cy.wait('@courses');
-
-        cy.get("mat-card").should("have.length", 9);
-
-    });
-
-    it('should display the advanced courses', () => {
-
-        cy.get('.mat-tab-label').should("have.length", 2);
-
-        cy.get('.mat-tab-label').last().click();
-
-        cy.get('.mat-tab-body-active .mat-card-title').its('length').should('be.gt', 1);
-
-        cy.get('.mat-tab-body-active .mat-card-title').first()
-            .should('contain', "Angular Security Course");
-
-    });
-
-
+    cy.get(".mat-tab-body-active .mat-card-title")
+      .first()
+      .should("contain", "Angular Security Course");
+  });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
